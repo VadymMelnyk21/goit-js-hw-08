ab9216015a7219a61bd7f69f4a4e34b37e02b1fb
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-16 23:32:25 +0200

    Задание 1 - библиотека SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector('.gallery');
+
+galleryContainerRef.insertAdjacentHTML('beforeend', renderGallery(galleryItems));
+
+
+function renderGallery(arrayOfGallery) {
+    
+    const markup = arrayOfGallery
+        .map(({ preview, original, description }) => `
+                <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}"  />
+                </a>   
+        `)
+        .join(''); 
+    
+    return markup;
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
