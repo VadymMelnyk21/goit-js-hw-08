e53172d6fe5d6e6e7d6fb9f2394b80185fd93980
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-17 01:03:47 +0200

    метод setCurrentTime()

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,6 +5,11 @@ const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const stopTime = localStorage.getItem(STORAGE_KEY);
+if (stopTime) {
+    player.setCurrentTime(stopTime)
+};
+
 player.on('timeupdate', elem => {
     localStorage.setItem(STORAGE_KEY, elem.seconds)
 });
