b2be2a2e9935a64e685099b3c776be4c9e647546
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-17 00:48:24 +0200

    ключ для хранилища

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,11 @@
 import Player from '@vimeo/player';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on('timeupdate', elem => {
-
+    localStorage.setItem(STORAGE_KEY, elem.seconds)
 });
 
