b794b9ee741a184f396c673b464f8fca311f97ef
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-18 00:43:16 +0200

    хранилище данных

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,6 +2,8 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 
+savedDataForm();
+
 form.addEventListener('input', throttle(inputData, 500));
 
 function inputData(event) {
@@ -12,4 +14,15 @@ function inputData(event) {
     formData[event.target.name] = event.target.value;
 
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+ 
+function savedDataForm() {
+    let savedData = localStorage.getItem('feedback-form-state');
+    if (savedData) {
+        savedData = JSON.parse(savedData);
+    }
+    
+    Object.entries(savedData).forEach(
+        ([name, value]) => (form.elements[name].value = value),
+    )
  };
