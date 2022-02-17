4ae86fd12de4cc58434b01d2c088f4f4dc976a47
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-18 00:17:46 +0200

    отслеживание input

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,8 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(inputData, 500));
+
+function inputData(event) {
+ }
