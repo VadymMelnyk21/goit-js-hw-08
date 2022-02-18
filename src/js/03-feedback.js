1e64b399aa9e1850ef3855783ea59d72bd64fbfe
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-18 02:34:02 +0200

    отрефакторил код

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,40 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const FORMA_KEY = 'feedback-form-state';
+const formDataInput = {};
 
 savedDataForm();
 
 form.addEventListener('input', throttle(inputData, 500));
 form.addEventListener('submit', submitData)
 
-function inputData(event) {
-    let formDataInput = localStorage.getItem('feedback-form-state');
-
-    formDataInput = formDataInput ? JSON.parse(formDataInput) : {};
-
-    formDataInput[event.target.name] = event.target.value;
-
-    localStorage.setItem('feedback-form-state', JSON.stringify(formDataInput));
+function inputData() {
+    formDataInput.email = form.elements.email.value;
+    formDataInput.message = form.elements.message.value;
+    localStorage.setItem(FORMA_KEY, JSON.stringify(formDataInput));
 };
  
 function submitData(event) {
     event.preventDefault();
 
+    if (form.elements.email.value === '' || form.elements.message.value === '') {
+        alert('Заполните все поля');
+        return;
+    };
+
     const formData = new FormData(form);
     formData.forEach((key, value) => console.log(`${value}:${key}`));
 
     event.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');
+    localStorage.removeItem(FORMA_KEY);
 };
  
 function savedDataForm() {
-    let savedData = localStorage.getItem('feedback-form-state');
-    if (savedData) {
-        savedData = JSON.parse(savedData);
-    }
-
+    let savedData = localStorage.getItem(FORMA_KEY);
+    
+    savedData = savedData ? JSON.parse(savedData) : {};
+    
     Object.entries(savedData).forEach(
         ([name, value]) => (form.elements[name].value = value),
     )
